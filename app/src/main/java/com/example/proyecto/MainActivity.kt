d75f128340f1550c6352a9e934294505e3975878
Mau Rdz <1930mau@gmail.com>
2022-09-29 01:46:32 -0500

    Agregar spinner y correcciones en la foto

--- app/src/main/java/com/example/proyecto/MainActivity.kt
+++ app/src/main/java/com/example/proyecto/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.proyecto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var spin: Spinner
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        spin = findViewById(R.id.spinner)
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.Dificultad,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spin.adapter = adapter
+        }
+
+
     }
 }
